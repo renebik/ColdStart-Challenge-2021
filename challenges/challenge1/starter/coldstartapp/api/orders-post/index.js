0a16d52643717d955131c46ebd636eb369d3d9dc
René Bik <Rene.Bik@codit.eu>
2021-03-01 22:15:49 +0100

    added store to queue

--- challenges/challenge1/starter/coldstartapp/api/orders-post/index.js
+++ challenges/challenge1/starter/coldstartapp/api/orders-post/index.js
@@ -1,4 +1,5 @@
 const { getUser } = require('../shared/user-utils');
+var azure = require('azure-storage');
 
 module.exports = async function (context, req) {
   // Get the user details from the request
@@ -7,6 +8,16 @@ module.exports = async function (context, req) {
   // Get the pre-order from the request
   
   // TODO: add the pre-order JSON document in a queue
-
+  var queueSvc = azure.createQueueService();
+  queueSvc.createQueueIfNotExists('myqueue', function(error, results, response){
+    if(!error){
+      // Queue created or exists
+      queueSvc.createMessage('myqueue', "Hello, World", function(error, results, response){
+        if(!error){
+          // Message inserted
+        }
+      });
+    }
+  });
   context.res.status(201);
 };
