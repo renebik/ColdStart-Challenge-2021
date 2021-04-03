9cdffadceeb3aab6fe57356a69cff352fa3764e5
René Bik <Rene.Bik@codit.eu>
2021-04-03 14:22:55 +0200

    updated config

--- challenges/challenge3/starter/coldstartapp/api/shared/config.js
+++ challenges/challenge3/starter/coldstartapp/api/shared/config.js
@@ -3,15 +3,15 @@ const process = require("process");
 
 const config = {
   azure_storage_connectionstring: process.env.AZURE_STORAGE_CONNECTIONSTRING,
-  personalizer_key: process.env.PERSONALIZER_KEY,
-  personalizer_baseuri: process.env.PERSONALIZER_BASEURI,
+  personalizer_key: process.env.PERSONALIZER_API_KEY,
+  personalizer_baseuri: process.env.PERSONALIZER_ENDPOINT,
 };
 
 const sqlConfig = {
-  user: process.env.SQL_USERNAME,
-  password: process.env.SQL_PASSWORD,
-  server: process.env.SQL_SERVERNAME,
-  database: 'coldstartsql',
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_DATABASE,
   pool: {
     max: 10,
     min: 0,
